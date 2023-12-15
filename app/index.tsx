53309818c7e959ed09a395ea6fb625fde9152000
Gouldy-C <mail4clg@gmail.com>
2023-12-14 23:48:55 -0600

    ran the setup for firebase and auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import {StyleSheet} from 'react-native'
-import React from 'react'
+
 import { Redirect } from 'expo-router'
+import React, { useEffect } from 'react';
+import auth from '@react-native-firebase/auth';
+import { userStore } from '../utils/stores/userStore';
+import { useLoading } from '../utils/stores/loadingStore';
 
 
 const AppEntry = () => {
+  const {loading, setLoading} = useLoading((state) => state);
+  const {user, setUser} = userStore((state) => state);
+
+  // Handle user state changes
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (loading) setLoading(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (loading) return null;
+
+  if (!user) {
+    return (
+      <Redirect  href={'/(auth)/sign-in'}/>
+    );
+  }
+
   return (
-    <Redirect href={"/sign-up"}/>
-  )
+    <Redirect  href={'/(user)/feed'}/>
+  );
 }
 
 export default AppEntry
-
-const styles = StyleSheet.create({
-})
