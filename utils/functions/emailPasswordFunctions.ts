ae8d630dfd25080ce8d2df095036d2a4d675ddac
Gouldy-C <mail4clg@gmail.com>
2023-12-15 00:04:01 -0600

    ran the setup for firebase and auth

---
+++ utils/functions/emailPasswordFunctions.ts
@@ -0,0 +1,52 @@
+import auth from '@react-native-firebase/auth';
+import { router } from 'expo-router';
+
+export const signUpEmailPassword = (email : string, password : string) => { 
+  auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+      console.log(result);
+      console.log('User account created & signed in!');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+
+      console.error(error);
+    })
+  }
+
+export const signInEmailPassword = (email : string, password : string) => { 
+  auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      console.log(result);
+      console.log('User account created & signed in!');
+    })
+    .catch(error => {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+
+      console.error(error);
+    })
+  }
+
+  export const signOutUser = () => { 
+    auth()
+      .signOut()
+      .then(() => {
+        console.log('User signed out!')
+        router.replace('/(auth)/sign-in')
+      })
+      .catch((err) => console.log(err))
+    }
