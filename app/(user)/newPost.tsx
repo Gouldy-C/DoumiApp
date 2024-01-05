88cd0de0eb22211f7e2135ef1119d4bfa499dcc0
Gouldy-C <mail4clg@gmail.com>
2024-01-05 13:36:39 -0800

    added imput validation for the post

--- app/(user)/newPost.tsx
+++ app/(user)/newPost.tsx
@@ -1,34 +1,50 @@
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native'
+import {Text, View, StyleSheet, TextInput, Button, Pressable} from 'react-native'
 import React, { useState } from 'react'
 import { handlePost } from '@utils/posting/functions';
 import { router } from 'expo-router';
+import { ZodType, z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import ControlledTextInput from '@components/ControlledTextInput';
+
+
+type FormData = {
+  post: string;
+};
 
 const NewPost = () => {
-  
-  const [ inputValue, setInputValue ] = useState('')
-  
-  const handleInputChange = (text:string) => {
-    setInputValue(text);
-  };
+  const schema: ZodType<FormData> = z.object({
+    post: z
+      .string()
+      .min(2)
+      .max(1000)
+  });
 
-  const submitPost = async () => {
-    handlePost(inputValue)
-    setInputValue('')
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const submitData = async (data: FormData) => {
+    handlePost(data.post)
     router.push('/(user)/(feed)/userFeed')
   };
 
   return (
       <View style={styles.safeView}>
         <View style={styles.container}>
-          <TextInput
-            style={styles.input}
-            placeholder='Type something...'
-            onChangeText={handleInputChange}
-            value={inputValue}
+          <ControlledTextInput
+            control={control}
+            placeholder={"Your Post"}
+            name={"post"}
+            label={"New Post"}
           />
-        </View>
-        <View style={styles.button}>
-          <Button title="Post" onPress={submitPost} />
+          <Pressable onPress={handleSubmit(submitData)}>
+            <Text style={styles.button}>Post</Text>
+          </Pressable>
         </View>
       </View>
   )
@@ -38,23 +54,22 @@ export default NewPost
 
 const styles = StyleSheet.create({
   safeView: {
-    marginTop: 30,
     flex: 1,
     alignItems: "center",
   },
   container: {
+    marginVertical: 20,
     width: "80%",
-    height: 40,
     alignSelf: "center",
-    borderColor: 'black',
-    borderWidth: 1,
     paddingLeft: 1,
   },
-  input: {
-    backgroundColor: 'white',
-    height: '100%'
-  },
   button: {
-    marginTop: 10
+    textAlign: "center",
+    fontSize: 20,
+    margin: 10,
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
   },
 })
