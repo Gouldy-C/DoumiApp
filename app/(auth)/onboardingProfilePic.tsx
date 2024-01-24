cf333ff63a10552c761ccb3892386d06cf38b750
Gouldy-C <mail4clg@gmail.com>
2024-01-24 00:06:54 -0800

    added profile images and firebase storage bucket

--- app/(auth)/onboardingProfilePic.tsx
+++ app/(auth)/onboardingProfilePic.tsx
@@ -1,11 +1,11 @@
-import { ScrollView, Text, View } from 'react-native'
+import { Pressable, ScrollView, Text, View } from 'react-native'
+import { Image } from 'expo-image';
 import React from 'react'
 import DoumiOnboardingHeader from '@components/DoumiOnboardingHeader'
 import DefaultPurpleButton from '@components/DefaultPurpleButton'
 import { userStore } from '@utils/stores/userStore'
-import storage from '@react-native-firebase/storage'
+import { profileImages } from '@constants/profileImages'
 
-const ref = storage().ref('gs://caretaker-app-colab25.appspot.com/Profile_Images/butterfly.png')
 
 const onboardingProfilePic = () => {
   const setProfilePic = userStore((state) => state.setProfilePic)
@@ -14,11 +14,23 @@ const onboardingProfilePic = () => {
 
 
   return (
-    <ScrollView style={{}} contentContainerStyle={{paddingVertical: 76, width: '100%', alignItems: 'center', backgroundColor: '#ffffff', flex: 1}}>
+    <ScrollView style={{}} contentContainerStyle={{paddingVertical: 76, width: '100%', alignItems: 'center', backgroundColor: '#ffffff', flex: 1, paddingHorizontal: 16}}>
       <DoumiOnboardingHeader textValue="What would you like to be called?" textSize='large' />
       <Text style={{fontSize: 19, color: '#424052', textAlign: 'center' }}>Pick an anonymous profile picture</Text>
       <Text style={{fontSize: 19, color: '#424052', textAlign: 'center', marginVertical: 24}}>You can always change it in the app.</Text>
         <View style={{alignItems: 'center', width: '100%', paddingVertical: 16}}>
+          {profileImages.map((picUrl) => (
+              <Pressable onPress={() => {}}>
+                <Image
+                  style={{height: 50, width: 50}}
+                  source={picUrl}
+                  placeholder={'Profile picture'}
+                >
+
+                </Image>
+              </Pressable>
+            ))
+          }
           
           <DefaultPurpleButton styles={{}} onPress={() => {}} disabled={false}>
             <Text style={{color: '#ffffff', fontSize: 18, fontWeight: '700'}}>Create Account</Text>
--- constants/profileImages.ts
+++ constants/profileImages.ts
@@ -2,14 +2,14 @@
 
 
 export const profileImages = [
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/butterfly.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/dog.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/knitted.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/elephant.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/parrot.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/rooster.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/swine.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/teddy-bear.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/whale.png',
-  'gs://caretaker-app-colab25.appspot.com/Profile_Images/whole-fish.png'
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fbutterfly.png?alt=media&token=1ea07342-4f5e-4124-a1c1-b7691bb3634f',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fdog.png?alt=media&token=468a5162-e56e-44f5-91ae-3b96e14fd0ca',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Felephant.png?alt=media&token=da1851c6-6caa-4e05-954b-2f7ec8276e0b',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fknitted.png?alt=media&token=1de48e56-a275-4fbe-91d5-a66971fa6fc8',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fparrot.png?alt=media&token=458f23ad-9fdd-463a-86e7-3c31ea752e3e',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Frooster.png?alt=media&token=0fd4b4ab-c2f4-43f3-aa91-4da80b4381ee',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fswine.png?alt=media&token=89a575c5-ca88-4b60-a93d-ad4324b9d567',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fteddy-bear.png?alt=media&token=b29b4026-fda6-491f-948e-d2e462b6d071',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fwhale.png?alt=media&token=61ec4fb7-5748-487b-accd-7338d0bf0b79',
+  'https://firebasestorage.googleapis.com/v0/b/caretaker-app-colab25.appspot.com/o/Profile_Images%2Fwhole-fish.png?alt=media&token=c3fcbe90-aa27-4dfd-9dba-79adb4917502'
 ]
