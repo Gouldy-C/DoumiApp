f3cd3538c88b4cec6d1e765002237c23acd21216
Gouldy-C <mail4clg@gmail.com>
2024-01-25 16:03:39 -0800

    user login is in a functioning state

--- constants/strategiesData/strategiesData.ts
+++ constants/strategiesData/strategiesData.ts
@@ -12,15 +12,15 @@ export const strategies = [
     title: "Get On Their Level",
     description:"Try backing away and making sure the area is quiet. Crouch down, so you can easily make direct eye contact.\n\nThe lower you are, the smaller you'll appear. This is much less threatening than looming over someone.",
     source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
-    categories: ["Agitated Behavior", "Depressed Mood"],
+    categories: ["Agitated Behavior"],
     strategyId: "02d16e7e-628d-4514-b081-b32c3d76c2c0",
     image: GetOnLevel,
   },
   {
     title: "Maintain Routine",
-    description:"Strategy 2",
-    source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
-    categories: ["Memory Care", "Depressed Mood", "Agitated Behavior"],
+    description:"Reduce stress by increasing predictability in the day. Consider their likes, dislikes, and past routines to establish a schedule. Maintain regular activities like meals, but also throw in some interesting stuff like playing music, doing puzzles, or even taking a walk together. Be flexible, because things can change.\n\nFor instance, if they seem bored, mix in something new or take a break. The idea is to balance activities and rest, so it's enjoyable for them. You could have mornings for getting ready, having coffee, and doing crafts, afternoons for gardening, visiting friends, and relaxing, and evenings for dinner, playing cards, or watching a movie. The goal is to focus on their joy and sense of accomplishment, and reduce anxiety through planning ahead.",
+    source: "Alzheimer's Association",
+    categories: ["Agitated Behavior"],
     strategyId: "a0e7231a-e27b-4b7d-bc41-2c31099491db",
     image: MaintainRoutine,
   },
@@ -28,7 +28,7 @@ export const strategies = [
     title: "Bring Out Old Photographs",
     description:"Strategy 3",
     source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
-    categories: ["Agitated Behavior", "Hygiene Trouble"],
+    categories: ["Agitated Behavior"],
     strategyId: "a54f6970-bbd0-4653-bdfa-299d24f35b85",
     image: OldPhotos,
   },
@@ -36,7 +36,8 @@ export const strategies = [
     title: "Clear The Room",
     description:
       "Strategy 4",
-    categories: ["Memory Care", "Hygiene Trouble", "Agitated Behavior"],
+    source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
+    categories: ["Agitated Behavior"],
     strategyId: "f2b590a9-9634-4b5f-9db7-1f825c19634c",
     image: ClearRoom,
   },
@@ -44,6 +45,7 @@ export const strategies = [
     title: "Check For Loud Noises",
     description:
       "Strategy 5",
+    source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
     categories: ["Agitated Behavior"],
     strategyId: "f10e4fc5-cd4e-4f8f-ab0b-6178819ba3ad",
     image: LoudNoises,
@@ -52,6 +54,7 @@ export const strategies = [
     title: "Validate And Reassure",
     description:
       "Strategy 6",
+    source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
     categories: ["Agitated Behavior"],
     strategyId: "fe4d55f6-1dca-46e3-afff-c438a0883d22",
     image: ValidateReassure,
@@ -60,8 +63,165 @@ export const strategies = [
     title: "Control The Climate",
     description:
       "Strategy 7",
+    source: "Teepa Snow's 10 Steps to De-Escalating a Dementia Care Crisis",
     categories: ["Agitated Behavior"],
     strategyId: "ed52d93a-ae10-46ee-a62a-1c736c207668",
     image: ControlClimate,
+  },
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  {
+    title: "Plan ahead",
+    description:
+      "Preparing the bathroom in advance is crucial. Caregivers can gather supplies, make the room comfortable, and monitor water temperature to ensure a safe environment. Safety measures, such as nonskid mats and grab bars, should be in place.\n\nBathing can be an overwhelming sensory experience. Consider all of the five senses - Are you using the same products, so they smell familiar? Is the lighting soothing, yet bright enough so everyone can see clearly? Is there a bathrobe and/or a large towel to wrap up in for warmth and privacy?",
+    source: "Alzheimer's Association",
+    categories: ["Hygiene Trouble"],
+    strategyId: "d12af427-8b16-4736-acfe-1148b2082b2e",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Give them a choice",
+    description:
+      'Helping the person feel in control is another key strategy. Caregivers can provide choices, involve the individual in the bathing process, and be sensitive to their preferences. Ask questions, instead of making demands. For example, you can say, "Are you ready to wash up?" instead of "Are you ready to take a bath?"\n\nExperimentation may be necessary to determine whether showers or tub baths are preferred. Distraction techniques, like playing soothing music, can help alleviate resistance.',
+    source: "Alzheimer's Association",
+    categories: ["Hygiene Trouble"],
+    strategyId: "e2fd7fa7-1ffa-42b7-b13c-3b68fcf3dc3d",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Adapting a routine",
+    description:
+      "Adapting the bathing process is essential as dementia progresses. Establishing a routine, using simple phrases and cues, and employing adaptive tools like tub benches contribute to a smoother bathing experience. Washing hair and maintaining personal hygiene should be approached with care and consideration.",
+    source: "Alzheimer's Association",
+    categories: ["Hygiene Trouble"],
+    strategyId: "0cf8f901-356b-4ebf-a1a0-be51fa3db0f7",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Consider alternatives",
+    description:
+      "You may need to pivot occasional from showers and baths. Adjust your standards and explore options like washing specific body parts on different days or using non-rinse soap products. Seeking assistance from trained caregivers or nursing assistants can be beneficial.",
+    source: "Alzheimer's Association",
+    categories: ["Hygiene Trouble"],
+    strategyId: "fdbc8069-7fe9-4225-89fa-5dad9e9ca301",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Shampoo separately",
+    description:
+      "Have you ever had an unexpected bucket of water poured over your head? It may have made you jump, and you probably didn't hold your breath in time. Likewise, dementia patients can get scared when they're forced under a shower and water is poured over their head. Having that negative connection to bathing may make it harder to get them back in the bathroom the next day.\n\nInstead, consider washing their hair in a portable basin, so it's separate from bathing.",
+    source: "BetterHealth Victoria",
+    categories: ["Hygiene Trouble"],
+    strategyId: "cb07a4e7-b79c-47fe-848e-df92c11c13cb",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  {
+    title: "Schedule a predictable routine",
+    description:
+      "Schedule a predictable daily routine, taking advantage of the person's best time of day to undertake difficult tasks, such as bathing.",
+    source: "Alzheimer's Association",
+    categories: ["Depressed Mood"],
+    strategyId: "ad3e8262-d9bd-40e9-bd89-85aa8f60b3aa",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Do enjoyable activities together",
+    description:
+      "Make a list of activities, people or places that the person enjoys and schedule these things more frequently. Nurture the person with offers of favorite foods or soothing or inspirational activities.",
+    source: "Alzheimer's Association",
+    categories: ["Depressed Mood"],
+    strategyId: "82f28e88-ab20-4545-a4fd-d89e1be99eba",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Regular Exercise",
+    description:
+      "Help the person exercise regularly, particularly in the morning.",
+    source: "Alzheimer's Association",
+    categories: ["Depressed Mood"],
+    strategyId: "d75fa3dd-c7bb-4bac-9ded-4c1f668ec386",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Acknowledge frustration",
+    description:
+      "Acknowledge the person's frustration or sadness, while continuing to express hope that he or she will feel better soon.",
+    source: "Alzheimer's Association",
+    categories: ["Depressed Mood"],
+    strategyId: "ef0df8ee-c595-4344-bd4b-47379455ee20",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Make them feel loved",
+    description:
+      "Provide reassurance that the person is loved, respected and appreciated as part of the family, and not just for what she or he can do now.",
+    source: "Alzheimer's Association",
+    categories: ["Depressed Mood"],
+    strategyId: "642d5cfa-894e-4045-ae98-4adafdffc056",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  {
+    title: "Sing together",
+    description:
+      "Encourage your loved one to play their instrument or sing and do sing-along of hymns, holiday carols, or carol songs.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "84bea475-fb9a-44d1-b0f9-bb6db724596e",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Play well-loved footages",
+    description:
+      "Play home videos of your loved ones, well-loved shows and movies from the 40s through the 80s, recorded concerts and events that include historical milestones to inspire treasured memories to surface.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "b909631f-ea07-4e6f-a083-d9277b5d975b",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Problem solve together",
+    description:
+      "Offer puzzles, sensory boxes or boards to explore, workbooks with brain games that encourage problem-solving.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "825fe935-a8af-4718-8e64-7c7c3241be73",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Go through familiar items",
+    description:
+      "Fill a prop box with a photo album, a treasured Christmas ornament, a favorite painting, a favorite clothing, family voice recordings, and home videos.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "7a08cc1e-c0fb-471f-ba3a-514a105de2d7",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Let them touch their favorite things",
+    description:
+      "Inspire reminiscence about special events by letting your loved one touch their beloved, personal items such as a beaded wedding dress or favorite pair of cufflinks. Encourage them to close eyes and identify an object such as stuffed bear by touch.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "cfa0ad08-3c6e-4e74-89f8-b6e556772f1a",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Smell nice things",
+    description:
+      "Let your loved one guess the scents of essential oils, smell their favorite coffee, or a familiar smell.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "9fc51ee7-2329-410e-a256-5150bc1093dc",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
+  },
+  {
+    title: "Eat or cook to bring back memories",
+    description:
+      "Bake a classic gingerbread recipe to inspire holiday memories or use scents like chocolate chip cookies or apple pie to stimulate appetite.",
+    source: "A Place for Mom",
+    categories: ["Memory Care"],
+    strategyId: "196ce940-ba80-49b4-a954-9fb8be3a239d",
+    image: 'https://mtek3d.com/wp-content/uploads/2018/01/image-placeholder-500x500-300x300.jpg',
   },
 ];
